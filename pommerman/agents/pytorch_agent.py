67253bf764d342b420190eab0c68bf4c0fd8b386
JacobPjetursson <jacob@pjetursson.com>
2018-10-29 14:50:32 +0100

    Initial files for agents and run-scripts

---
+++ pommerman/agents/pytorch_agent.py
@@ -0,0 +1,16 @@
+'''The base simple agent use to train agents.
+This agent is also the benchmark for other agents.
+'''
+from collections import defaultdict
+import queue
+import random
+
+import numpy as np
+
+from . import BaseAgent
+from .. import constants
+from .. import utility
+
+
+class PytorchAgent(BaseAgent):
+    pass
