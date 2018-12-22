f12d28de0898e28e536dd84a814fa1751b0edc72
Zaggralez <jacob@pjetursson.com>
2018-12-22 15:38:43 +0100

    now logging number of timesteps and reward for both training and evaluation
    indent in spaces pls

--- src/main.py
+++ src/main.py
@@ -24,203 +24,207 @@ lr_update_schedule = None if args.lr_schedule is None else args.lr_schedule // u
 
 torch.manual_seed(args.seed)
 if args.cuda:
-	torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
 np.random.seed(args.seed)
 
 try:
-	os.makedirs(args.log_dir)
+    os.makedirs(args.log_dir)
 except OSError:
-	files = glob.glob(os.path.join(args.log_dir, '*.monitor.csv'))
-	try:
-		for f in files:
-			os.remove(f)
-	except:
-		pass
+    files = glob.glob(os.path.join(args.log_dir, '*.monitor.csv'))
+    try:
+        for f in files:
+            os.remove(f)
+    except:
+        pass
 
 eval_log_dir = args.log_dir + "_eval"
 try:
-	os.makedirs(eval_log_dir)
+    os.makedirs(eval_log_dir)
 except OSError:
-	files = glob.glob(os.path.join(eval_log_dir, '*.monitor.csv'))
-	try:
-		for f in files:
-			os.remove(f)
-	except:
-		pass
+    files = glob.glob(os.path.join(eval_log_dir, '*.monitor.csv'))
+    try:
+        for f in files:
+            os.remove(f)
+    except:
+        pass
 
 
 def main():
-	torch.set_num_threads(1)
-	device = torch.device("cuda:0" if args.cuda else "cpu")
+    torch.set_num_threads(1)
+    device = torch.device("cuda:0" if args.cuda else "cpu")
 
-	train_envs = make_vec_envs(
-		args.env_name, args.seed, args.num_processes, args.gamma, args.no_norm, args.num_stack,
-		args.log_dir, args.add_timestep, device, allow_early_resets=False)
+    train_envs = make_vec_envs(
+        args.env_name, args.seed, args.num_processes, args.gamma, args.no_norm, args.num_stack,
+        args.log_dir, args.add_timestep, device, allow_early_resets=False)
 
-	if args.eval_interval:
-		eval_envs = make_vec_envs(
-			args.env_name, args.seed + args.num_processes, args.num_processes, args.gamma,
-			args.no_norm, args.num_stack, eval_log_dir, args.add_timestep, device,
-			allow_early_resets=True, eval=True)
+    if args.eval_interval:
+        eval_envs = make_vec_envs(
+            args.env_name, args.seed + args.num_processes, args.num_processes, args.gamma,
+            args.no_norm, args.num_stack, eval_log_dir, args.add_timestep, device,
+            allow_early_resets=True, eval=True)
 
-		if eval_envs.venv.__class__.__name__ == "VecNormalize":
-			eval_envs.venv.ob_rms = train_envs.venv.ob_rms
-	else:
-		eval_envs = None
+        if eval_envs.venv.__class__.__name__ == "VecNormalize":
+            eval_envs.venv.ob_rms = train_envs.venv.ob_rms
+    else:
+        eval_envs = None
 
-	# FIXME this is very specific to Pommerman env right now
-	# Husk at vi har et lidt andet space, gider ikke rode rundt i det for at se hvor man skal hen
+    # FIXME this is very specific to Pommerman env right now
+    # Husk at vi har et lidt andet space, gider ikke rode rundt i det for at se hvor man skal hen
 
-	actor_critic = create_policy(
-		train_envs.observation_space,
-		train_envs.action_space,
-		name='pomm',
-		nn_kwargs={
-			'batch_norm': True,
-			'recurrent': False,
-			'hidden_size': 512,
-		},
-		train=True)
-	if args.load_path and load:
-		print("Loading in previous model")
-		try:
-			path_ = "./trained_models/a2c/PommeFFACompetitionFast-v0.pt"
-			if args.algo.startswith('ppo'):
-				path_ = "./trained_models/ppo/PommeFFACompetitionFast-v0.pt"
-#			state_dict, ob_rms = torch.load(args.load_path)
-			state_dict, ob_rms = torch.load(path_)
-			actor_critic.load_state_dict(state_dict)
-		except:
-			print("Wrong path!")
-			exit(1)
-	actor_critic.to(device)
+    actor_critic = create_policy(
+        train_envs.observation_space,
+        train_envs.action_space,
+        name='pomm',
+        nn_kwargs={
+            'batch_norm': True,
+            'recurrent': False,
+            'hidden_size': 512,
+        },
+        train=True)
+    if args.load_path and load:
+        print("Loading in previous model")
+        try:
+            path_ = "./trained_models/a2c/PommeFFACompetitionFast-v0.pt"
+            if args.algo.startswith('ppo'):
+                path_ = "./trained_models/ppo/PommeFFACompetitionFast-v0.pt"
+            #			state_dict, ob_rms = torch.load(args.load_path)
+            state_dict, ob_rms = torch.load(path_)
+            actor_critic.load_state_dict(state_dict)
+        except:
+            print("Wrong path!")
+            exit(1)
+    actor_critic.to(device)
 
-	if args.algo.startswith('a2c'):
-		agent = algo.A2C_ACKTR(
-			actor_critic, args.value_loss_coef,
-			args.entropy_coef,
-			lr=args.lr, lr_schedule=lr_update_schedule,
-			eps=args.eps, alpha=args.alpha,
-			max_grad_norm=args.max_grad_norm)
-	elif args.algo.startswith('ppo'):
-		agent = algo.OUR_PPO( # PPO HER!
-			actor_critic, args.clip_param, args.ppo_epoch, args.num_mini_batch,
-			args.value_loss_coef, args.entropy_coef,
-			lr=args.lr, lr_schedule=lr_update_schedule,
-			eps=args.eps,
-			max_grad_norm=args.max_grad_norm)
-	elif args.algo == 'acktr':
-		agent = algo.A2C_ACKTR(
-			actor_critic, args.value_loss_coef,
-			args.entropy_coef,
-			acktr=True)
+    if args.algo.startswith('a2c'):
+        agent = algo.A2C_ACKTR(
+            actor_critic, args.value_loss_coef,
+            args.entropy_coef,
+            lr=args.lr, lr_schedule=lr_update_schedule,
+            eps=args.eps, alpha=args.alpha,
+            max_grad_norm=args.max_grad_norm)
+    elif args.algo.startswith('ppo'):
+        agent = algo.OUR_PPO(  # PPO HER!
+            actor_critic, args.clip_param, args.ppo_epoch, args.num_mini_batch,
+            args.value_loss_coef, args.entropy_coef,
+            lr=args.lr, lr_schedule=lr_update_schedule,
+            eps=args.eps,
+            max_grad_norm=args.max_grad_norm)
+    elif args.algo == 'acktr':
+        agent = algo.A2C_ACKTR(
+            actor_critic, args.value_loss_coef,
+            args.entropy_coef,
+            acktr=True)
 
-	rollouts = RolloutStorage(
-		args.num_steps, args.num_processes,
-		train_envs.observation_space.shape,
-		train_envs.action_space,
-		actor_critic.recurrent_hidden_state_size)
+    rollouts = RolloutStorage(
+        args.num_steps, args.num_processes,
+        train_envs.observation_space.shape,
+        train_envs.action_space,
+        actor_critic.recurrent_hidden_state_size)
 
-	obs = train_envs.reset()
-	rollouts.obs[0].copy_(obs)
-	rollouts.to(device)
+    obs = train_envs.reset()
+    rollouts.obs[0].copy_(obs)
+    rollouts.to(device)
 
-	episode_rewards = deque(maxlen=10)
-	
-	start = time.time()
-	for j in range(num_updates):
-		for step in range(args.num_steps):
-			# Sample actions
-			with torch.no_grad():
-				value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
-						rollouts.obs[step],
-						rollouts.recurrent_hidden_states[step],
-						rollouts.masks[step])
+    episode_rewards = deque(maxlen=10)
 
-			# Obser reward and next obs
-			obs, reward, done, infos = train_envs.step(action)
+    start = time.time()
+    for j in range(num_updates):
+        for step in range(args.num_steps):
+            # Sample actions
+            with torch.no_grad():
+                value, action, action_log_prob, recurrent_hidden_states = actor_critic.act(
+                    rollouts.obs[step],
+                    rollouts.recurrent_hidden_states[step],
+                    rollouts.masks[step])
 
-			for info in infos:
-				if 'episode' in info.keys():
-					rew = info['episode']['r']
-					#length = info['episode']['l']
-					#if rew < 0:
-					#	rew = rew * (1 + (float(length) / 800))
-					#if rew > 0:
-					#	rew = rew * (1 + float(800 - length) / 800)
-					episode_rewards.append(rew)
+            # Obser reward and next obs
+            obs, reward, done, infos = train_envs.step(action)
 
-			# If done then clean the history of observations.
-			masks = torch.tensor([[0.0] if done_ else [1.0] for done_ in done], device=device)
-			rollouts.insert(obs, recurrent_hidden_states, action, action_log_prob, value, reward, masks)
+            for info in infos:
+                if 'episode' in info.keys():
+                    rew = info['episode']['r']
+                    # length = info['episode']['l']
+                    # if rew < 0:
+                    #	rew = rew * (1 + (float(length) / 800))
+                    # if rew > 0:
+                    #	rew = rew * (1 + float(800 - length) / 800)
+                    episode_rewards.append(rew)
 
-		with torch.no_grad():
-			next_value = actor_critic.get_value(rollouts.obs[-1],
-												rollouts.recurrent_hidden_states[-1],
-												rollouts.masks[-1]).detach()
+            # If done then clean the history of observations.
+            masks = torch.tensor([[0.0] if done_ else [1.0] for done_ in done], device=device)
+            rollouts.insert(obs, recurrent_hidden_states, action, action_log_prob, value, reward, masks)
 
-		rollouts.compute_returns(next_value, args.use_gae, args.gamma, args.tau)
+        with torch.no_grad():
+            next_value = actor_critic.get_value(rollouts.obs[-1],
+                                                rollouts.recurrent_hidden_states[-1],
+                                                rollouts.masks[-1]).detach()
 
-		value_loss, action_loss, dist_entropy, other_metrics = agent.update(rollouts, j)
+        rollouts.compute_returns(next_value, args.use_gae, args.gamma, args.tau)
 
-		rollouts.after_update()
+        value_loss, action_loss, dist_entropy, other_metrics = agent.update(rollouts, j)
 
-		if j % args.save_interval == 0 and args.save_dir != "":
-			save_path = os.path.join(args.save_dir, args.algo)
-			try:
-				os.makedirs(save_path)
-			except OSError:
-				pass
+        rollouts.after_update()
 
-			# Save model
-			save_model = actor_critic
-			if args.cuda:
-				save_model = copy.deepcopy(actor_critic).cpu()
+        if j % args.save_interval == 0 and args.save_dir != "":
+            save_path = os.path.join(args.save_dir, args.algo)
+            try:
+                os.makedirs(save_path)
+            except OSError:
+                pass
 
-			save_model = [save_model.state_dict(),
-							hasattr(train_envs.venv, 'ob_rms') and train_envs.venv.ob_rms or None]
+            # Save model
+            save_model = actor_critic
+            if args.cuda:
+                save_model = copy.deepcopy(actor_critic).cpu()
 
-			torch.save(save_model, os.path.join(save_path, args.env_name + ".pt"))
+            save_model = [save_model.state_dict(),
+                          hasattr(train_envs.venv, 'ob_rms') and train_envs.venv.ob_rms or None]
 
-		total_num_steps = (j + 1) * update_factor
+            torch.save(save_model, os.path.join(save_path, args.env_name + ".pt"))
 
-		if j % args.log_interval == 0 and len(episode_rewards) > 1:
-			end = time.time()
-			print("Updates {}, num timesteps {}, FPS {}, last {} mean/median reward {:.1f}/{:.1f}, "
-				  "min / max reward {:.1f}/{:.1f}, value/action loss {:.5f}/{:.5f}".
-				format(j, total_num_steps,
-					   int(total_num_steps / (end - start)),
-					   len(episode_rewards),
-					   np.mean(episode_rewards),
-					   np.median(episode_rewards),
-					   np.min(episode_rewards),
-					   np.max(episode_rewards), dist_entropy,
-					   value_loss, action_loss), end=', ' if other_metrics else '\n')
+        total_num_steps = (j + 1) * update_factor
 
-		if args.eval_interval and len(episode_rewards) > 1 and j > 0 and j % args.eval_interval == 0:
-			eval_episode_rewards = []
+        if j % args.log_interval == 0 and len(episode_rewards) > 1:
+            end = time.time()
+            print("Updates {}, num timesteps {}, FPS {}, last {} mean/median reward {:.1f}/{:.1f}, "
+                  "min / max reward {:.1f}/{:.1f}, value/action loss {:.5f}/{:.5f}".
+                  format(j, total_num_steps,
+                         int(total_num_steps / (end - start)),
+                         len(episode_rewards),
+                         np.mean(episode_rewards),
+                         np.median(episode_rewards),
+                         np.min(episode_rewards),
+                         np.max(episode_rewards), dist_entropy,
+                         value_loss, action_loss), end=', ' if other_metrics else '\n')
+            with open("train_results_" + args.algo + ".txt", "a+") as res_file:
+                to_print = "{},{}".format(total_num_steps, np.mean(episode_rewards))
+                res_file.write(to_print)
 
-			obs = eval_envs.reset()
-			eval_recurrent_hidden_states = torch.zeros(args.num_processes,
-							actor_critic.recurrent_hidden_state_size, device=device)
-			eval_masks = torch.zeros(args.num_processes, 1, device=device)
+        if args.eval_interval and len(episode_rewards) > 1 and j > 0 and j % args.eval_interval == 0:
+            eval_episode_rewards = []
 
-			while len(eval_episode_rewards) < 50:
-				with torch.no_grad():
-					_, action, _, eval_recurrent_hidden_states = actor_critic.act(
-						obs, eval_recurrent_hidden_states, eval_masks, deterministic=True)
+            obs = eval_envs.reset()
+            eval_recurrent_hidden_states = torch.zeros(args.num_processes,
+                                                       actor_critic.recurrent_hidden_state_size, device=device)
+            eval_masks = torch.zeros(args.num_processes, 1, device=device)
 
-				# Obser reward and next obs
-				obs, reward, done, infos = eval_envs.step(action)
-				eval_masks = torch.tensor([[0.0] if done_ else [1.0] for done_ in done], device=device)
-				for info in infos:
-					if 'episode' in info.keys():
-						eval_episode_rewards.append(info['episode']['r'])
-			with open("results_"+args.algo+".txt", "a+") as res_file:
-				to_print = "{},{}".format(len(eval_episode_rewards), np.mean(eval_episode_rewards))
-				res_file.write(to_print)
-			print("Evaluation using {} episodes: mean reward {:.5f}\n".format(len(eval_episode_rewards), np.mean(eval_episode_rewards)))
+            while len(eval_episode_rewards) < 50:
+                with torch.no_grad():
+                    _, action, _, eval_recurrent_hidden_states = actor_critic.act(
+                        obs, eval_recurrent_hidden_states, eval_masks, deterministic=True)
+
+                # Obser reward and next obs
+                obs, reward, done, infos = eval_envs.step(action)
+                eval_masks = torch.tensor([[0.0] if done_ else [1.0] for done_ in done], device=device)
+                for info in infos:
+                    if 'episode' in info.keys():
+                        eval_episode_rewards.append(info['episode']['r'])
+            with open("eval_results_" + args.algo + ".txt", "a+") as res_file:
+                to_print = "{},{}".format(total_num_steps, np.mean(eval_episode_rewards))
+                res_file.write(to_print)
+            print("Evaluation using {} episodes: mean reward {:.5f}\n".format(len(eval_episode_rewards),
+                                                                              np.mean(eval_episode_rewards)))
 
 
 if __name__ == "__main__":
-	main()
+    main()
