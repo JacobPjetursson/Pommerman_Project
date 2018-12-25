74c8818c27738e3d62db1233ed29919c8ae96bb8
Zaggralez <jacob@pjetursson.com>
2018-12-25 15:29:01 +0100

    GRAPHS BABY

---
+++ src/make_graph.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+from decimal import Decimal
+
+train_timesteps = []
+train_results = []
+eval_timesteps = []
+eval_results = []
+train_obs_skip = 100
+with open("train_results_a2c.txt", "r") as f:
+    mean_res = 0.0
+    i = 0
+    for line in f.readlines():
+        timestep, result = line.split(",")
+        mean_res += float(result)
+        if i % train_obs_skip == 0 and i:
+            timestep = '%.2E' % Decimal(timestep)
+            train_timesteps.append(timestep)
+            mean_res /= train_obs_skip
+            train_results.append(round(mean_res, 2))
+            mean_res = 0
+        i += 1
+
+fig, ax = plt.subplots(figsize=(12, 7))
+ax.plot(train_timesteps, train_results)
+plt.xticks(rotation=90)
+ax.set_xticks(ax.get_xticks()[::10])
+ax.set_xlabel("Timesteps")
+plt.gcf().subplots_adjust(bottom=0.17)
+ax.set_ylabel("Reward")
+fig.suptitle("Train results")
+plt.savefig("results/train_results.png")
+plt.show()
+
+with open("eval_results_a2c.txt", "r") as f:
+    for line in f.readlines():
+        timestep, result = line.split(",")
+
+        timestep = '%.2E' % Decimal(timestep)
+        result = round(float(result), 2)
+        eval_timesteps.append(timestep)
+        eval_results.append(result)
+
+fig, ax = plt.subplots(figsize=(12, 7))
+ax.plot(eval_timesteps, eval_results)
+plt.xticks(rotation=90)
+ax.set_xticks(ax.get_xticks()[::10])
+ax.set_xlabel("Timesteps")
+plt.gcf().subplots_adjust(bottom=0.17)
+ax.set_ylabel("Reward")
+fig.suptitle("Evaluation results")
+plt.savefig("results/eval_results.png")
+plt.show()
